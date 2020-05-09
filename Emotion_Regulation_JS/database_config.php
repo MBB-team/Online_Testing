2d801d80e36d1364d8e0f4ef7aae14e7b7535378
Gilles <gilles.rautureau@icm-institute.org>
2020-05-09 19:46:56 +0200

    implement runID in task Emotion_Regulation_JS
    add a loader file file to create a run (linking participant to
        taskSession)
    replace subject_id by run_id in tableEmo
    remove all subject_id references in experiment_RSVP.html
    update write_data.php to insert runID
    TODO: call endTask
    
    side changes :
    use sessionOpenDataBase() and common session_database_config.php

--- Emotion_Regulation_JS/database_config.php
+++ Emotion_Regulation_JS/database_config.php
@@ -1,14 +1,20 @@
 <?php
-// Change all the informations as needed
 
-  $servername = 'mariadb-docker';
-  $port = 3306; //
-  $username = 'root'; // this is the default for XAMPP.
-  $password = '67jyt645ez5LFJIyrege456g48r';
-  $dbname = 'databaseEmo';
+  // common database parameters moved to
+  // portailLib/database_config_session.php
+  // see portailLib/database_config_session_template.php
+  // use :
+  //
+  // include('../portailLib/session.php');
+  // include('database_config.php'); //just for table name
+  // $conn = sessionOpenDataBase()
+  //
+  // instead of :
+  //
+  // include('database_config.php');
+  // $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+  // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
   $table = "tableEmo";
 
-  $db = new mysqli($servername, $username, $password, $dbname) or die("Unable to connect");
-
-  // echo "Success, connected to my databaseEmo MySQL with PHP"
 ?>
