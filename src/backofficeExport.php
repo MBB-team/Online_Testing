0d6d14821a1fd4e896d64a929d907e66dc8f4083
Gilles <gilles.rautureau@icm-institute.org>
2020-05-13 11:40:04 +0200

    simple or joined export with dropdown list

--- src/backofficeExport.php
+++ src/backofficeExport.php
@@ -2,21 +2,34 @@
 
 include('portailLib/backoffice.php');
 
-$table = $onlyDone = "";
+$table = $onlyDone = $exportType = "";
 //form processing
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $table = test_input("table");
     $onlyDone = test_input("onlyDone");
+    $exportType = test_input("exportType");
 }
+
+
+$errorMessage = "";
 
 if($table!="")
 {
     //export CSV
-    exportCSV($table, $onlyDone=="true");
-    exit();
+    switch($exportType)
+    {
+        case "simple":
+            $errorMessage = exportCSVSimpleTable($table);
+            break;
+        case "joined":
+            $errorMessage = exportCSVJoinedTable($table, $onlyDone=="true");
+            break;
+    }
+
 }
-else
-{
+
+
+
     //display form
     echo '<!DOCTYPE html>
     <html>
@@ -25,14 +38,47 @@ else
         </head>
         <body>';
 
+    if($errorMessage!="")
+    {
+        echo "<p style='color:red'>".$errorMessage."</p>";
+    }
+
+    //simple export
+    echo "<p>Export d'une table</p>";
     echo "<form method='post' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "'>
-        <p>Table à exporter : <input type='input' name='table'></p>
+        <input type='hidden' name='exportType' value='simple'>
+        <p>Table à exporter : ";
+
+    displayDropDownList('table', getAllTables());
+    echo "</p>";
+    echo "<input type='submit' value='Exporter'>
+    </form>";
+
+    echo "<br><br>";
+
+    //joined dataTask export
+    echo "<p>Export des données pour une tâche (table de la tâche + taskSession + run)</p>";
+    echo "<form method='post' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "'>
+        <input type='hidden' name='exportType' value='joined'>
+        <p>Table de tâche à exporter : ";
+
+    displayDropDownList('table', getDataTaskTables());
+    echo "</p>
         <p>Exclure les run incomplets : <input type='checkbox' name='onlyDone' value='true'></p>
         <input type='submit' value='Exporter'>
         </form>";
 
     echo '</body>
     </html>';
+
+function displayDropDownList($fieldName, $items)
+{
+    echo "<select name='".$fieldName."'>";
+    foreach($items as $item)
+    {
+        echo "<option value='".$item."'>".$item."</option>";
+    }
+    echo "</select>";
 }
 
 function test_input($data) {
--- src/portailLib/backoffice.php
+++ src/portailLib/backoffice.php
@@ -1,6 +1,40 @@
 <?php
 
-function exportCSV($table, $onlyDone = false)
+// If no error, echo a CSV file for one table if no error.
+// Else return an error string.
+function exportCSVSimpleTable($table)
+{
+    //check if table exist
+    if(!in_array($table, getAllTables()))
+        return $table." n'existe pas dans la base de données";
+
+    //SQL request for one table
+    $sql = "SELECT * FROM ".$table;
+    return exportCSV($sql, $table."_".date('Y-m-d_H-i-s'));
+}
+
+// If no error, echo a CSV file for a data task table joined with run and taskSession if no error.
+// Else return an error string.
+function exportCSVJoinedTable($table, $onlyDone=false)
+{
+    //check if table is a data task table
+    if(!in_array($table, getDataTaskTables()))
+        return $table." n'existe pas dans la base de données";
+    //SQL request for this table joined with run and taskSession
+    $sql = "SELECT * FROM run, taskSession, ".$table;
+    $sql.= " WHERE runID=run_id AND taskSessionID=taskSession_taskSessionID";
+    //if $onlyDone. Filter only done run
+    if($onlyDone)
+    {
+        $sql.=" AND 'doneTime' IS NOT NULL";
+    }
+    return exportCSV($sql, $table."_joined"."_".date('Y-m-d_H-i-s'));
+}
+
+// subfunction to export CSV.
+// actually do the SQL request and form the CSV file
+// If error return a string error.
+function exportCSV($sql, $csvFilename="export")
 {
     $separator = "\t";
 
@@ -9,29 +43,19 @@ function exportCSV($table, $onlyDone = false)
         // connect to database
         $conn = backofficeOpenDataBase();
 
-        // Get all opened tasks
-        $sql = "SELECT * FROM ".$table;
-        // $sql.= " WHERE runID=run_id AND participantID=participant_participantID AND taskSessionID=taskSession_taskSessionID";
-        //if $taskID is supplied. Filter only this task
-        if($onlyDone)
-        {
-            $sql.=" AND 'doneTime' IS NOT NULL";
-        }
         $exportStmt = $conn->prepare($sql);
 
         $exportStmt->execute();
         if(!$exportStmt->execute())
         { //fail
-            echo 'Something went wrong. Check table name.';
-            exit();
+            return 'Something went wrong. Check table name.';
         }
 
         $exportSQL = $exportStmt->fetchAll();
 
         if(count($exportSQL)<=0)
         { //no data
-            echo 'Empty result :(';
-            exit();
+            return 'Empty result';
         }
 
         $csvExport = "";
@@ -65,8 +89,9 @@ function exportCSV($table, $onlyDone = false)
         }
 
         header("Content-type: text/x-csv");
-        header("Content-Disposition: attachment; filename=\"export_".$table.".csv\"");
+        header("Content-Disposition: attachment; filename=\"".$csvFilename.".csv\"");
         echo($csvExport);
+        exit();
 
     }
     catch(PDOException $e)
@@ -76,6 +101,50 @@ function exportCSV($table, $onlyDone = false)
     }
 }
 
+//return names of tables containing tasks data 
+function getDataTaskTables()
+{
+    //Note : Might be better and flexible to add a field in task with the associated data table name
+
+    $tableList = array( "table_DPD",
+                        "tableSE",
+                        "tableQuest",
+                        "tableEmo",
+                        "tableConstance"
+                      );
+    return $tableList;
+}
+
+function getAllTables()
+{
+    try {
+
+        // connect to database
+        $conn = backofficeOpenDataBase();
+
+        // Get all tables in database
+        $sql = "SHOW TABLES";
+        $tablesListStmt = $conn->prepare($sql);
+
+        $tablesListStmt->execute();
+
+        $tablesFetch = $tablesListStmt->fetchAll(PDO::FETCH_NUM);
+
+        $tablesList = array();
+
+        //parse each line to form a simpler array
+        foreach($tablesFetch as $table)
+        {
+            array_push($tablesList,$table[0]);
+        }
+        return $tablesList;
+    }
+    catch(PDOException $e)
+    {
+        print "Erreur !:" . $e->getMessage() . "<br/>";
+    }
+
+}
 
 function backofficeOpenDataBase()
 {
