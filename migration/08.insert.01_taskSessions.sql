c8df9f7595e79a5a6614b3eb477fe77fccc65274
Gilles Rautureau <gilles.rautureau@icm-institute.org>
2020-07-10 19:15:43 +0200

    Add test sessions

---
+++ migration/08.insert.01_taskSessions.sql
@@ -0,0 +1,15 @@
+SET NAMES utf8;
+
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'CONST_S5', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'CONST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'DPD_S5', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'DPD');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'EMOREG_S7', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'EMOREG');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S5', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'QUEST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'SE_S5', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'SE');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'ENGMAIN_S2', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'ENGMAIN');
+
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S08', '2020-07-13 08:00:00', '2020-07-13 09:59:59', 'QUEST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S10', '2020-07-13 10:00:00', '2020-07-13 11:59:59', 'QUEST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S12', '2020-07-13 12:00:00', '2020-07-13 13:59:59', 'QUEST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S14', '2020-07-13 14:00:00', '2020-07-13 15:59:59', 'QUEST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S16', '2020-07-13 16:00:00', '2020-07-13 17:59:59', 'QUEST');
+INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S18', '2020-07-13 18:00:00', '2020-07-13 19:59:59', 'QUEST');
