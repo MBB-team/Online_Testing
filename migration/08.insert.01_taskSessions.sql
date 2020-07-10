811d099106aa8e441294a6350291bdeaa95414ec
Gilles Rautureau <gilles.rautureau@icm-institute.org>
2020-07-10 19:25:05 +0200

    adjust schedule of test sessions

--- migration/08.insert.01_taskSessions.sql
+++ migration/08.insert.01_taskSessions.sql
@@ -8,8 +8,5 @@ INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closi
 INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'ENGMAIN_S2', '2020-07-11 00:00:00', '2020-07-12 23:59:59', 'ENGMAIN');
 
 INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S08', '2020-07-13 08:00:00', '2020-07-13 09:59:59', 'QUEST');
-INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S10', '2020-07-13 10:00:00', '2020-07-13 11:59:59', 'QUEST');
 INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S12', '2020-07-13 12:00:00', '2020-07-13 13:59:59', 'QUEST');
-INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S14', '2020-07-13 14:00:00', '2020-07-13 15:59:59', 'QUEST');
 INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S16', '2020-07-13 16:00:00', '2020-07-13 17:59:59', 'QUEST');
-INSERT INTO `taskSession` (`taskSessionID`, `sessionName`, `openingTime`, `closingTime`, `task_taskID`) VALUES (NULL, 'QUEST_S18', '2020-07-13 18:00:00', '2020-07-13 19:59:59', 'QUEST');
