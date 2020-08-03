d81f26f67460e2a3c5dd63f9a82f62c9ebfe36d9
william-hopper <57996698+william-hopper@users.noreply.github.com>
2020-08-03 09:45:49 +0100

    created V2 plugin + script

--- src/EngMain/jsPsych-master/plugins_WH/jspsych-html-keyboard-response-WH-EM-V2.js
+++ src/EngMain/jsPsych-master/plugins_WH/jspsych-html-keyboard-response-WH-EM-V2.js
@@ -57,23 +57,10 @@ jsPsych.plugins["html-keyboard-response-WH-EM-V2"] = (function() {
       },
       target: {
         type: jsPsych.plugins.parameterType.INT,
-        pretty_name: 'Target side',
+        pretty_name: 'Initial target side',
         array: true,
         default: undefined,
-        description: 'The side that the target stimulus is currently expected to appear. 0 = left; 1 = right'
-      },
-      grid: {
-        type: jsPsych.plugins.parameterType.BOOL,
-        pretty_name: 'Grid',
-        array: true,
-        default: [[1,1,1,1]],
-        description: 'This array represents the grid of boxes shown on the screen.'
-      },
-      grid_square_size: {
-        type: jsPsych.plugins.parameterType.INT,
-        pretty_name: 'Grid square size',
-        default: 100,
-        description: 'The width and height in pixels of each square in the grid.'
+        description: 'The inital side that the target stimulus is expected to appear. 0 = left; 1 = right'
       },
       target_trial: {
         type: jsPsych.plugins.parameterType.INT,
@@ -94,6 +81,12 @@ jsPsych.plugins["html-keyboard-response-WH-EM-V2"] = (function() {
         pretty_name: 'Number of Stimuli',
         default: 232,
         description: 'The number of stimuli in a trial'
+      },
+      difficulty: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial Difficulty',
+        default: undefined,
+        description: 'The difficulty of the trial (Easy (0) or Hard (1))'
       }
     }
   }
@@ -101,20 +94,157 @@ jsPsych.plugins["html-keyboard-response-WH-EM-V2"] = (function() {
   plugin.trial = function(display_element, trial) {
 
     var nbStim = 0;
+    var target_side = trial.target;
+    var switch_stim;
+    var new_html;
+    var update_stim = 1;
+    var response = {
+      rt: null,
+      key: null
+    };
     display_stim(); // initial stim
-    var stim_interval = setInterval(display_stim, trial.trial_duration);
+
+    var stim_interval = setInterval(display_stim, trial.trial_duration); // set interval
 
     // display stimulus
-    function (){
-      if (nbImg == trial.number_stim){
+    function display_stim(){
+      if (nbStim == trial.number_stim){
         clearInterval(stim_interval);
         end_trial();
-      } else if (nbStim == trial.optout_question_index[0] || nbStim == trial.optout_question_index[1]){
-
       } else {
+        if (nbStim == trial.optout_question_index[0] || nbStim == trial.optout_question_index[1]){ // if we need to display the explicit optout question
+          if (update_stim){ // only update the optout HTML once
+            new_html = "<p style='font-size: 50px'>Voulez-vous arre&#770ter cet exercice ?</p><p>Si oui, appuyez la touche <b><< Entre&#769e >></b> !</p>";
 
-      }
-    }
+            display_element.innerHTML = new_html;
+
+            var optout_keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
+              callback_function: function(info){
+
+                // only record first response
+                if (response.key == null){
+                  response = info;
+                }
+
+                // if opted out
+                if (info.key == 13){
+                  end_trial();
+                }
+
+              },
+              valid_responses: 13,
+              rt_method: 'performance',
+              persist: false,
+              allow_held_key: false
+            });
+
+
+            var optout_question = jsPsych.pluginAPI.setTimeout(function(){
+              switch (target_side) {
+                case 0:
+                new_html = "<p style='font-size: 100px'><</p>"
+                break;
+                case 1:
+                new_html = "<p style='font-size: 100px'>></p>"
+                break;
+              }
+
+              display_element.innerHTML = new_html;
+
+              // resume
+              jsPsych,pluginAPI.setTimeout(function(){nbStim++; update_stim = 1;}, 500);
+
+            },4000)
+
+          } // update stim
+
+        } else if (!document.fullscreenElement || !document.mozFullScreenElement || !document.webkitFullscreenElement || !document.msFullscreenElement){ // check fullscreen before display rsvp stim
+
+          display_element.innerHTML = "Vous devez \352tre en mode plein \351cran pour continuer l'exp\351rience!  <br></br> Veuillez cliquer sur le bouton ci-dessous pour passer en mode plein \351cran.<br></br><p>" + '<button id="jspsych-fullscreen-btn" class="jspsych-btn">Continuer</button>';
+          var listener = display_element.querySelector('#jspsych-fullscreen-btn').addEventListener('click', function() {
+            var element = document.documentElement;
+            if (element.requestFullscreen) {
+              element.requestFullscreen();
+            } else if (element.mozRequestFullScreen) {
+              element.mozRequestFullScreen();
+            } else if (element.webkitRequestFullscreen) {
+              element.webkitRequestFullscreen();
+            } else if (element.msRequestFullscreen) {
+              element.msRequestFullscreen();
+            }
+          });
+
+
+        } else { // display stim
+
+          new_html = trial.stimulus;
+
+          // add prompt
+          if(trial.prompt !== null){
+            new_html += trial.prompt;
+          }
+
+          // draw
+          display_element.innerHTML = new_html;
+
+          // align text appropriately
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-0-1").style.verticalAlign = "bottom";
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-1-0").style.textAlign     = "right";
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-2-1").style.verticalAlign = "top";
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-0-5").style.verticalAlign = "bottom";
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-1-6").style.textAlign     = "left";
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-2-5").style.verticalAlign = "top";
+
+          // set spacing cells
+          var spacing = ""+sizeInDeg-70+"px"
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-0-2").style.width = spacing;
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-0-4").style.width = spacing;
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-1-2").style.width = spacing;
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-1-4").style.width = spacing;
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-2-2").style.width = spacing;
+          display_element.querySelector("#jspsych-RSVP-EM-stimulus-cell-2-4").style.width = spacing;
+
+          var distractor_streams = display_element.querySelectorAll("div[stream = distractor]");
+
+          // insert distractor content
+          for (var distr_i = 0; distr_i < distractor_streams.length; distr_i++){
+            distractor_streams[distr_i].innerHTML = trial.stimulus[distr_i+3][nbStim];
+          }
+
+          // insert target streams content
+          display_element.querySelectorAll("div[stream = target]")[target_side].innerHTML = trial.stimulus[0][nbStim];
+          display_element.querySelectorAll("div[stream = target]")[1 - target_side].innerHTML = trial.stimulus[2][nbStim];
+
+          // insert switch Content
+          switch_stim = trial.stimulus[1][nbStim]
+
+          if (switch_stim == 3){ // if this is a switch stim
+            if (!trial.difficulty){ // if its NOT HARD, need to change 3s to arrows
+              if (target_side == 0){
+                switch_stim = '<';
+              } else if (target_side == 1){
+                switch_stim = '>';
+              }
+            }
+            target_side = 1 - target_side;
+          }
+
+          display_element.querySelector("div[stream = switch]").innerHTML = switch_stim;
+
+          nbStim++; // iterate the stim
+
+        } // if not fullscreen or optout but display stim
+      } // if not last stim
+    }; // display_stim
+
+
+
+
+
+
+
+
+    ////////////////////======================================================
     // add prompt
     if(trial.prompt !== null){
       new_html += trial.prompt;
