0c2d0008670809c93cbb9b638953c3dffde41fc5
Gilles Rautureau <gilles.rautureau@icm-institute.org>
2020-08-18 20:47:29 +0200

    update ENGMAIN wo session

--- src/EngMain/experiment_EM.php
+++ src/EngMain/experiment_EM.php
@@ -106,6 +106,7 @@ Data Output (as in MySQL):
 
           // --------------------------------- INITIALISATION  --------------------------- //
           dataSaver = new DataSaver(dataSaverModes.SERVER, 'write_data.php');
+          dataSaver.SetClientIds(<?php echoAsJsArray($clientIds); ?>);
 
           // Checks if the browser is Chrome or Firefox (best compatibility)
           var browserInfo = getBrowserInfo();
@@ -326,7 +327,7 @@ Data Output (as in MySQL):
                           .then(() => console.log("Document Exited form Full screen mode"))
                           .catch((err) => console.error(err))
                     }
-                    endTask();
+                    setTimeout(function(){endTask()},2100); //wait for last async request end before retry
                 }
               });
             } // end of startExperiment
