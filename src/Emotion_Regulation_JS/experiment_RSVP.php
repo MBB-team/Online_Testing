de261b912292d4860c609bef7adccb17437b8d58
Gilles Rautureau <gilles.rautureau@icm-institute.org>
2020-08-18 20:24:52 +0200

    update EMOREG wo session

--- src/Emotion_Regulation_JS/experiment_RSVP.php
+++ src/Emotion_Regulation_JS/experiment_RSVP.php
@@ -69,6 +69,7 @@
 
 // --------------------------------- INITIALISATION  ---------------------------//
       dataSaver = new DataSaver(dataSaverModes.SERVER, 'write_data.php');
+      dataSaver.SetClientIds(<?php echoAsJsArray($clientIds); ?>);
 
     // Checks if the browser is Chrome or Firefox (best compatibility)
     var browserInfo = getBrowserInfo();
@@ -282,7 +283,7 @@
                                     .then(() => console.log("Document Exited form Full screen mode"))
                                     .catch((err) => console.error(err))
                               }
-                              endTask();
+                              setTimeout(function(){endTask()},2100); //wait for last async request end before retry
                         },
                   });
             }
