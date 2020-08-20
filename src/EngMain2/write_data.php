959a0aa116a668abcde0eb2e314a0c6062591d6b
Gilles Rautureau <gilles.rautureau@icm-institute.org>
2020-08-20 14:03:30 +0200

    Faster unlock php session on write data

--- src/EngMain2/write_data.php
+++ src/EngMain2/write_data.php
@@ -2,6 +2,7 @@
 
 //include PHP session management and fonctions to interract with user part of database
 include('../portailLib/session.php');
+session_write_close(); //unlock other php script using current session
 
 // this path should point to your configuration file.
 include('database_config.php');
