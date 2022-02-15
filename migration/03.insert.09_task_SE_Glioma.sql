365bd13c456c9aa17936068176dacdf62e2286ff
Arnaud Couchet <arnaud.couchet@icm-institute.org>
2022-02-15 15:48:47 +0100

    Remove old migration folder.

--- migration/03.insert.09_task_SE_Glioma.sql
+++
@@ -1,3 +0,0 @@
-SET NAMES utf8;
-
-INSERT INTO `task` (`taskID`, `taskName`, `url`, `dataTableName`) VALUES ('SE_glioma', 'Test de métacognition', 'SE_glioma/index.php', 'tableSE_glioma');
--- migration/03.insert.10_task_QUEST_GLIOMA.sql
+++
@@ -1,3 +0,0 @@
-SET NAMES utf8;
-
-INSERT INTO `task` (`taskID`, `taskName`, `url`, `dataTableName`) VALUES ('QUEST_GLIOMA', 'Questionnaires', 'Questionnaires_Glioma/index.php', 'tableQuestGLIOMA');
