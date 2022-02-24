ea1cf88f8318af521c84748d99e364e69c694ecc
Arnaud Couchet <arnaud.couchet@icm-institute.org>
2022-02-24 12:45:10 +0100

    Test migration.

--- migration/02.insert.01_participant_USER1.sql
+++ migration/02.insert.01_participant_USER1.sql
@@ -3,5 +3,3 @@ SET NAMES utf8;
 DELETE FROM databaseEmo.participant WHERE participantID IN ('user1', 'user2');
 
 INSERT INTO databaseEmo.participant(participantID, active) VALUES('user1', 1);
-
-INSERT INTO databaseEmo.participant(participantID, active) VALUES('user2', 1);
