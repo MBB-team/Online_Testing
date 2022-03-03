06b09166cc479c8226d56f723332f68761c08f16
Arnaud Couchet <arnaud.couchet@icm-institute.org>
2022-03-03 12:10:39 +0100

    Trigger a new deployment from develop branch.

--- migration/02.insert.01_participant_USER1.sql
+++ migration/02.insert.01_participant_USER1.sql
@@ -5,3 +5,4 @@ DELETE FROM databaseEmo.participant WHERE participantID IN ('user1', 'user2');
 INSERT INTO databaseEmo.participant(participantID, active) VALUES('user1', 1);
 
 INSERT INTO databaseEmo.participant(participantID, active) VALUES('user2', 1);
+
