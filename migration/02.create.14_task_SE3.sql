a6b6f3496f870e15078efeb3a86e7a3ab9ceeb98
GillesRautureauICM <gilles.rautureau@icm-institute.org>
2024-07-15 11:30:00 +0200

    SE3 add data fields

--- migration/02.create.14_task_SE3.sql
+++ migration/02.create.14_task_SE3.sql
@@ -44,6 +44,10 @@ CREATE TABLE IF NOT EXISTS `tableSE3` (
   `finalNC` int(3) DEFAULT NULL,
   `success` int(3) DEFAULT NULL,
   `get_data` tinyint(1) DEFAULT NULL,
+  `euro_rew` FLOAT DEFAULT NULL,
+  `exCorrect` int(4) DEFAULT NULL
+  `pointsTotal` int(4) DEFAULT NULL,
+  `response` text DEFAULT NULL,
   `PartID` int(3) DEFAULT NULL,
   `Condition` int(3) DEFAULT NULL,
   `SessID` int(3) DEFAULT NULL,
